f85f8cf1ebc9f099c18d22e8c0581fea6be4cc67
EthanMcGuire <ethanfmcguire@gmail.com>
2026-03-06 07:59:31 -0500

    Created moving spike, and added turret art.

---
+++ assets/scripts/trap/moving_spike.gd
@@ -0,0 +1,31 @@
+extends Trap
+
+const BASE_OFFSET = -8.0;
+
+@export var returnTime := 1.0;
+@export var returnDelay := 0.25;
+@export var stabTime := 0.2;
+@export var stabDelay := 0.5;
+
+func _ready() -> void:
+	var tween;
+	
+	tween = get_tree().create_tween().set_loops(0);
+	
+	tween.tween_method(_setSpikeOffset, BASE_OFFSET, BASE_OFFSET + 16.0, returnTime).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD).set_delay(returnDelay);
+	tween.tween_callback(_playSound).set_delay(stabDelay);
+	tween.tween_method(_setSpikeOffset, BASE_OFFSET + 16.0, BASE_OFFSET, stabTime).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD);
+	
+func _setSpikeOffset(offset: float) -> void:
+	var height;
+	
+	height = 16.0 - (offset + (BASE_OFFSET * -1.0));	# Height will remain at 16 if offset = BASE_OFFSET
+	
+	%Area2D.position.y = offset;
+	%Sprite2D.region_rect = Rect2(0.0, 0.0, 16.0, height);
+
+func _playSound() -> void:
+	%AudioStreamPlayer2D.play();
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	_dealDamage(body, %Area2D.global_position)
--- assets/scripts/trap/spike.gd
+++ assets/scripts/trap/spike.gd
@@ -1,5 +1,4 @@
 extends Trap
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	_dealDamage(body, %Area2D.global_position)
